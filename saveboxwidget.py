27c91d12d987d6b0b9a031a974825ecd8564403c
jagerber48 <justin.gerber48@gmail.com>
2020-09-12 20:29:00 -0700

    cleaning up code in saveboxwidget.py and setting up default initialization

--- saveboxwidget.py
+++ saveboxwidget.py
@@ -25,20 +25,17 @@ class SaveBoxWidget(QWidget, Ui_SaveBoxWidget):
         super(SaveBoxWidget, self).__init__(parent=parent)
         self.setupUi(self)
 
-        self.data_root = ''
-        self.date_path = ''
-        self.run_name = ''
-        self.camera_name = ''
-        self.file_number = ''
+        self.data_root_path = self.default_root
+        data_root_string = get_abbreviated_path_string(self.data_root_path, max_len=30)
+        self.data_root_value_label.setText(data_root_string)
+        self.daily_data_path = None
+        self.run_path = None
+        self.data_path = None
+        self.file_path = None
+        self.file_number = 1
         self.file_prefix = ''
         self.file_suffix = '.h5'
         self.file_name = ''
-        self.run_path = None
-        self.save_path = None
-        self.file_path = None
-        self.data_root_path = None
-        self.data_path = None
-        # self.get_savepath()
 
         self.mode = Mode.SINGLE
 
@@ -46,19 +43,26 @@ class SaveBoxWidget(QWidget, Ui_SaveBoxWidget):
         self.run_name_lineEdit.editingFinished.connect(self.build_data_path)
         self.select_data_pushButton.clicked.connect(self.select_data_path)
 
+        self.file_prefix_lineEdit.editingFinished.connect(self.build_file_name)
+        self.file_number_spinBox.editingFinished.connect(self.build_file_name)
+        self.file_number_spinBox.valueChanged.connect(self.build_file_name)
+
         self.set_daily_data_path()
         self.set_run_path()
+        self.build_data_path()
+        self.build_file_name()
+        self.set_file_path()
 
     def select_data_root(self):
         selected_directory = QFileDialog.getExistingDirectory(None, "Select Directory")
         self.data_root_path = Path(selected_directory)
-        data_root_string = get_abbreviated_path_string(self.data_root_path, max_len=35)
+        data_root_string = get_abbreviated_path_string(self.data_root_path, max_len=30)
         self.data_root_value_label.setText(data_root_string)
         self.build_data_path()
 
     def set_daily_data_path(self):
         """
-        Return folder path in the format 'YYYY\MM\DD\data\'
+        Return folder path in the format 'YYYY\\MM\\DD\\data\\'
         """
         today = datetime.datetime.today()
         year = f'{today.year:4d}'
@@ -75,56 +79,25 @@ class SaveBoxWidget(QWidget, Ui_SaveBoxWidget):
         self.data_path = Path(self.data_root_path, self.daily_data_path, self.run_path)
         data_dir_label_string = get_abbreviated_path_string(self.data_path)
         self.data_dir_label.setText(data_dir_label_string)
+        self.set_file_path()
 
     def select_data_path(self):
         selected_directory = QFileDialog.getExistingDirectory(None, "Select Directory")
         self.data_path = Path(selected_directory)
-        data_path_string = get_abbreviated_path_string(self.data_path)
-        self.data_dir_label.setText(data_path_string)
+        data_dir_label_string = get_abbreviated_path_string(self.data_path)
+        self.data_dir_label.setText(data_dir_label_string)
+        self.set_file_path()
 
     def build_file_name(self):
         self.file_prefix = self.file_prefix_lineEdit.text()
         self.file_number = self.file_number_spinBox.value()
         self.file_suffix = '.h5'
-        self.file_path = Path(f'{self.file_prefix}_{self.file_number:05d}{self.file_suffix}')
-        self.file_path_label.setText(f'Next File Name: {str(self.file_path)}')
+        self.file_name = f'{self.file_prefix}_{self.file_number:05d}{self.file_suffix}'
+        self.file_path_label.setText(f'Next File Name: {self.file_name}')
+        self.set_file_path()
 
-    def get_full_file_path(self):
-        self.full_file_path = Path(self.data_path, self.file_name)
-        return self.full_file_path
-
-    def get_savepath(self):
-        self.data_root = Path(self.data_root_lineEdit.text())
-        self.date_path = self.get_date_path()
-        self.run_path = self.get_run_path(self.run_name_lineEdit.text())
-        self.camera_name = self.camera_name_lineEdit.text()
-        self.file_number = int(self.file_number_spinBox.text())
-        self.file_suffix = '.h5'
-
-        self.save_path = Path(self.data_root, self.run_name, self.camera_name)
-        self.file_name = f'jkam_capture_{self.file_number:05d}{self.file_suffix}'
-        self.file_path = Path(self.save_path, self.file_name)
-        self.file_path_label.setText(str(self.file_path))
-
-    @staticmethod
-    def get_date_path():
-        """
-        Return folder path in the format 'YYYY\MM\DD'
-        """
-        today = datetime.datetime.today()
-        year = f'{today.year:4d}'
-        month = f'{today.month:2d}'
-        day = f'{today.day:2d}'
-        date_path = Path(year, month, day)
-        return date_path
-
-    @staticmethod
-    def get_run_path(runname):
-        """
-        Data directory structure includes a 'data' folder in every daily folder
-        """
-        run_path = Path('data', runname)
-        return run_path
+    def set_file_path(self):
+        self.file_path = Path(self.data_path, self.file_name)
 
     def toggle_enable_editing(self, toggle_value):
         self.data_root_lineEdit.setEnabled(toggle_value)
@@ -140,7 +113,7 @@ class SaveBoxWidget(QWidget, Ui_SaveBoxWidget):
     def save_h5_single(self, frame, timestamp=None):
         if not self.save_path.exists():
             self.save_path.mkdir(parents=True)
-        with h5py.File(self.file_path, 'w') as hf:
+        with h5py.File(self.file_name, 'w') as hf:
             hf.create_dataset("frame", data=frame.astype('uint16'))
             if timestamp is not None:
                 hf.attrs['timestamp'] = timestamp.isoformat()
