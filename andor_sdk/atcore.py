f15d870d51216e245d6a21c72c9607e4100a0cba
E6 <ultracolde6@gmail.com>
2020-09-14 18:25:42 -0700

    changing path on atcore

--- andor_sdk/atcore.py
+++ andor_sdk/atcore.py
@@ -175,7 +175,7 @@ class ATCore(object):
         """)
 
         #self.lib = self.ffi.verify('#include "atcore.h"', include_dirs=["."], libraries=["atcore"])
-        self.lib = self.ffi.dlopen('andor_sdk//atcore.lib')
+        self.lib = self.ffi.dlopen('atcore')
         self.handle_return(self.lib.AT_InitialiseLibrary())
 
     def __del__(self):
