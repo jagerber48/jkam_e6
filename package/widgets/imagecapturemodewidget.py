560b165fbcff7cca1ff8216a9442c330e8fd67f9
jagerber48 <justin.gerber48@gmail.com>
2020-10-08 12:22:31 -0700

    improve multishot spinbox logical flow. Adding color changing background to indicate image views have not been updated with updated number of frames

--- package/widgets/imagecapturemodewidget.py
+++ package/widgets/imagecapturemodewidget.py
@@ -20,6 +20,7 @@ class ImageCaptureModeWidget(QWidget, Ui_ImageCaptureModeWidget):
 
         self.imaging_mode = ImagingMode.VIDEO
         self.image_capture_buttonGroup.buttonClicked.connect(self.set_imaging_mode)
+        self.multishot_spinBox.valueChanged.connect(self.spinbox_adjusted)
         self.set_imaging_mode()
 
     def set_imaging_mode(self):
@@ -31,6 +32,7 @@ class ImageCaptureModeWidget(QWidget, Ui_ImageCaptureModeWidget):
             self.imaging_mode = ImagingMode.FLUORESCENCE
         elif self.multishot_mode_radioButton.isChecked():
             self.imaging_mode = ImagingMode.MULTISHOT
+        self.multishot_spinBox.setStyleSheet("QLineEdit {background-color: #FFFFFF;}")
         self.state_set_signal.emit(self.imaging_mode)
 
     def continuous_enabled(self):
@@ -38,6 +40,7 @@ class ImageCaptureModeWidget(QWidget, Ui_ImageCaptureModeWidget):
         self.absorption_mode_radioButton.setEnabled(False)
         self.fluorescence_mode_radioButton.setEnabled(False)
         self.multishot_mode_radioButton.setEnabled(False)
+        self.multishot_spinBox.setEnabled(False)
         self.video_mode_radioButton.setChecked(True)
         self.imaging_mode = ImagingMode.VIDEO
         self.state_set_signal.emit(self.imaging_mode)
@@ -47,15 +50,24 @@ class ImageCaptureModeWidget(QWidget, Ui_ImageCaptureModeWidget):
         self.absorption_mode_radioButton.setEnabled(True)
         self.fluorescence_mode_radioButton.setEnabled(True)
         self.multishot_mode_radioButton.setEnabled(True)
+        self.multishot_spinBox.setEnabled(True)
+
 
     def started(self):
         self.video_mode_radioButton.setEnabled(False)
         self.absorption_mode_radioButton.setEnabled(False)
         self.fluorescence_mode_radioButton.setEnabled(False)
         self.multishot_mode_radioButton.setEnabled(False)
+        self.multishot_spinBox.setEnabled(False)
+
 
     def disarmed(self):
         self.video_mode_radioButton.setEnabled(True)
         self.absorption_mode_radioButton.setEnabled(True)
         self.fluorescence_mode_radioButton.setEnabled(True)
         self.multishot_mode_radioButton.setEnabled(True)
+        self.multishot_spinBox.setEnabled(True)
+
+    def spinbox_adjusted(self):
+        self.multishot_spinBox.setStyleSheet("QSpinBox {background-color: #FFAAAA;}")
+
